544dd77cb4f4fa4eab557e2617c4851bbb0242dd
Martin <martingauna2000@gmail.com>
2022-05-11 20:10:18 -0300

    navbar

--- test-ecommerce/src/App.js
+++ test-ecommerce/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar/Nabvar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
      <h1>Hola Mundo</h1>
     </div>
   );
